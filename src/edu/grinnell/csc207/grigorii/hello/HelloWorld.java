0c9e169dc0572e040745fa47c93c4b1e380edb5f
Roman <grigorii@grinnell.edu>
2014-09-24 22:57:51 -0500

    new file:   src/edu/grinnell/csc207/grigorii/hello/HelloWorld.java

---
+++ src/edu/grinnell/csc207/grigorii/hello/HelloWorld.java
@@ -0,0 +1,14 @@
+package edu.grinnell.csc207.grigorii.hello;
+
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println ("Hello, GitHub!");
+	}
+
+}
